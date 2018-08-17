e88df16887d03bef8eae85833e4e2ccb08978349
rk59W18a <42214911+rk59W18a@users.noreply.github.com>
2018-08-17 16:16:40 +0200

    Add files via upload

---
+++ SottoCategoria.java
@@ -0,0 +1,54 @@
+package dominio;
+
+import java.io.Serializable;
+
+public class SottoCategoria extends Categoria implements Serializable
+{
+	private static final long serialVersionUID = 1L;
+
+	private String nomeSottoC;
+    
+    public static final String DESCRIZIONE_SOTTOCATEGORIA = "Nome sottocategoria: %s\n\t";
+    public static final String ELENCO_RISORSE_VUOTO = "Al momento non sono presenti risorse\n";
+    public static final String INTESTAZIONE_RISORSE = "Risorse in essa contenute:\n";
+    
+   /**
+    * @post: elencoRisorse != null
+    */
+    public SottoCategoria(String ns)
+    {
+    	    super();
+    	    this.nomeSottoC= ns;
+    	    inizializzaElencoRisorse();
+    }
+    
+    public String getNome()
+    {
+    	    return nomeSottoC;
+    }
+    
+    /**
+     * @pre: elencoRisorse != null
+     */
+    public String toString()
+    {
+ 	   StringBuffer ris = new StringBuffer();
+ 	   ris.append(String.format(DESCRIZIONE_SOTTOCATEGORIA, nomeSottoC));
+ 	   
+ 	   if(getElencoRisorse().size() == 0)
+ 		   ris.append(ELENCO_RISORSE_VUOTO);
+ 	   else
+ 	   {
+ 	      ris.append(INTESTAZIONE_RISORSE);
+ 	      
+ 		  for(int i = 0; i < getElencoRisorse().size(); i++)
+ 	      {
+ 		     Risorsa r = getElencoRisorse().get(i);
+ 		     ris.append("\t\t" + (i+1) + ")"+ r.toString());
+ 	      }  
+ 	   }
+ 	   
+ 	   return ris.toString();
+    }
+    
+}
