a2a04e91b910067ee0e43f3ca7f70e81da08f932
rk59W18a <42214911+rk59W18a@users.noreply.github.com>
2018-08-19 13:48:42 +0200

    Add files via upload

---
+++ Categoria.java
@@ -0,0 +1,275 @@
+package dominio;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+
+/**
+ * @invariant: (elencoRisorse == null) || (elencoSottoCategorie == null)
+ */
+public class Categoria implements Serializable
+{
+	   //private static final long serialVersionUID = 1L;
+	   
+	   //private String nomeCategoria;
+	   protected ArrayList <Risorsa> elencoRisorse;
+	   //private ArrayList <SottoCategoria> elencoSottoCategorie;
+	   
+	   //private int numeroMaxGiorniPrestito;
+	   //private int numeroMaxGiorniProroga;
+	   //private int numeroGiorniRichiestaProroga;
+	   //private int numeroMaxRisorseInPrestito;
+
+	   //public static final String DESCRIZIONE_CATEGORIA_SEMPLICE = "Nome categoria: %s\nRisorse in essa contenute:\n";
+	   //public static final String DESCRIZIONE_CATEGORIA_COMPOSTA = "Nome categoria: %s\nSottocategorie in essa contenute:\n";
+	   //public static final String ELENCO_SOTTOCATEGORIE_VUOTO = "\tAl momento non sono presenti sottocategorie\n";
+	   //public static final String ELENCO_RISORSE_VUOTO = "\t\tAl momento non sono presenti risorse\n";
+
+	   public static final String RIC_PER_TITOLO = "titolo";
+	   public static final String RIC_PER_AUTORE_I = "autore_i";
+	   public static final String RIC_PER_GENERE = "genere";
+	   public static final String RIC_PER_ANNOPUB = "annoPub";
+	   public static final String RIC_PER_CASAED = "casaEditrice";
+	   public static final String RIC_PER_REGISTA = "regista";
+	   public static final String RIC_PER_ATTORE_I = "attore_i";
+	 
+	   /*
+	   public Categoria(String n, int numPres, int numMaxPro, int numRiPro, int numRis)
+	   {
+		   this.nomeCategoria = n;
+		   this.numeroMaxGiorniPrestito = numPres;
+		   this.numeroMaxGiorniProroga = numMaxPro;
+		   this.numeroGiorniRichiestaProroga = numRiPro;
+		   this.numeroMaxRisorseInPrestito = numRis;
+	   }
+	   
+	   public Categoria()
+	   {
+		   
+	   }
+	   
+	   public void inizializzaElencoRisorse()
+	   {
+		   elencoRisorse = new ArrayList <Risorsa> ();
+	   }
+	   
+	   public void inizializzaElencoSottoCategorie()
+	   {
+		   elencoSottoCategorie = new ArrayList <SottoCategoria> ();
+	   }
+	   
+	   public String getNome()
+	   {
+		   return nomeCategoria;
+	   }
+	   
+	   public int getNumeroMaxGiorniPrestito()
+	   {
+		   return numeroMaxGiorniPrestito;
+	   }
+	   
+	   public int getNumeroMaxGiorniProroga()
+	   {
+		   return numeroMaxGiorniProroga;
+	   }
+	   
+	   public int getNumeroGiorniRichiestaProroga()
+	   {
+		   return numeroGiorniRichiestaProroga;
+	   }
+	   
+	   public int getNumeroMaxRisorseInPrestito()
+	   {
+		   return numeroMaxRisorseInPrestito;
+	   }
+	     
+	   public ArrayList <Risorsa> getElencoRisorse()  
+	   {
+		   return elencoRisorse;
+	   }
+	   
+	   public ArrayList <SottoCategoria> getElencoSottoCategorie() 
+	   {
+		   return elencoSottoCategorie;
+	   }
+	   
+	   public Risorsa getRisorsa(String t)
+	   {
+		   for(int i = 0; i < elencoRisorse.size(); i++)
+		   {
+			   Risorsa r = elencoRisorse.get(i);
+			   if(r.getTitolo().equalsIgnoreCase(t))
+				   return r;
+		   }
+		   
+		   return null;
+	   }
+	    
+	   public void aggiungiRisorsa(Risorsa r)  
+	   {
+		   elencoRisorse.add(r);
+	   }
+	   
+	   public void rimuoviRisorsa(Risorsa r)  
+	   {
+		   elencoRisorse.remove(r);
+		   
+	   }
+	   
+	   public void aggiungiSottoCategoria(SottoCategoria sc)
+	   {
+		   elencoSottoCategorie.add(sc);
+	   }
+	   
+	   public boolean verificaPresenza(String t)
+	   {
+		  boolean presente = false;
+		   
+		  for(int i = 0; i < elencoSottoCategorie.size(); i++)
+		  {
+			  SottoCategoria sc = elencoSottoCategorie.get(i);
+			  
+			  for(int j = 0; j < sc.getElencoRisorse().size(); j++)
+			  {
+				  Risorsa r = sc.getElencoRisorse().get(j);
+				  
+				  if(r.getTitolo().equalsIgnoreCase(t))
+				  {
+					  presente = true;
+					  break;
+				  }	   
+			  }
+		   }
+		   return presente;
+	   }
+	   */
+	   
+	   public ArrayList <Risorsa> ricercaRisorsaInElenco(Object o, String cr)
+	   {
+		   ArrayList <Risorsa> risorseCercate = new ArrayList <>();
+	   	    
+	   	    for(int i = 0; i < elencoRisorse.size(); i++)
+	   	    {
+	   	    	    Risorsa r = elencoRisorse.get(i);
+	   	    	    
+	   	    	    switch(cr)
+	   	    	    {
+	   	    	        case RIC_PER_TITOLO: if(r.getTitolo().toLowerCase().indexOf(((String)o).toLowerCase()) > -1)
+	  	    	    	                               risorseCercate.add(r);
+	   	    	                             break;
+	   	    	                       
+	   	    	        case RIC_PER_AUTORE_I: if(((Libro)r).getAutore().toLowerCase().indexOf(((String)o).toLowerCase()) > -1)
+	     	                     	              risorseCercate.add(r);
+	                                           break;
+	   	    	                       
+	   	    	        case RIC_PER_GENERE: if(r.getGenere().equalsIgnoreCase((String)o))
+	   	    	        	                      risorseCercate.add(r);
+	   	    	                             break;
+	   	    	                    
+	   	    	        case RIC_PER_ANNOPUB: if(r.getAnnoPub() ==  (Integer)o)
+	     	                     	              risorseCercate.add(r);
+	   	    	        					  break;
+	                      
+	   	    	        case RIC_PER_CASAED: if(((Libro)r).getCasaEditrice().equalsIgnoreCase((String)o))
+	     	                     	               risorseCercate.add(r);
+	   	    	        					 break;
+	   	    	        					 
+	   	    	        case RIC_PER_REGISTA: if(((Film)r).getRegista().toLowerCase().indexOf(((String)o).toLowerCase()) > -1)
+	   	    	        							risorseCercate.add(r);
+	   	    	        					  break; 
+	                
+	   	    	        case RIC_PER_ATTORE_I: if(((Film)r).getAttore().toLowerCase().indexOf(((String)o).toLowerCase()) > -1)
+	   	    	        							risorseCercate.add(r);
+	   	    	        					   break;
+	   	    	    } 	     
+	   	    }
+	   	    
+	   	    return risorseCercate;
+	   }
+	   
+	   public ArrayList <Risorsa> ricercaRisorsa(Object o, String comeRicercare)
+	   {
+		   ArrayList <Risorsa> risorseCercate = new ArrayList <>();
+		   
+		   if(elencoSottoCategorie == null)
+		   { 
+			   risorseCercate = ricercaRisorsaInElenco(o, comeRicercare);
+		   }
+		   else
+		   {
+			   for(int i = 0; i < elencoSottoCategorie.size(); i++)
+			   {
+				   SottoCategoria sc = elencoSottoCategorie.get(i);
+				   risorseCercate.addAll(sc.ricercaRisorsaInElenco(o, comeRicercare));
+			   }
+		   }
+		   
+		   return risorseCercate;
+	   }
+	  
+	   public String stampaElencoRisorse()
+	   {
+	      StringBuffer ris = new StringBuffer();
+	 	   
+	 	   for(int i = 0; i < elencoRisorse.size(); i++)
+	 	   {
+	 		   Risorsa r = elencoRisorse.get(i);
+	 		   ris.append(i+1 + ")"+ r.getTitolo() + "\n");
+	 	   }
+	 	   
+	 	   return ris.toString();
+	   }
+	   
+	   public String stampaElencoSottocategorie()
+	   {
+		   StringBuffer ris = new StringBuffer();
+		   
+		   for(int i = 0; i < elencoSottoCategorie.size(); i++)
+		   {
+			   SottoCategoria sc = elencoSottoCategorie.get(i);
+			   ris.append(i+1 + ")" + sc.getNome() + "\n");
+		   }
+		   
+		   return ris.toString();
+	   }
+	   
+	   /*
+	   public String toString()
+	   {
+		   StringBuffer ris = new StringBuffer();
+		   
+		   if(elencoSottoCategorie == null)
+		   {
+		       ris.append(String.format(DESCRIZIONE_CATEGORIA_SEMPLICE, nomeCategoria));
+		   
+		       if(elencoRisorse.size() == 0)
+		    	       ris.append(ELENCO_RISORSE_VUOTO);
+		       else
+		       {
+		          for(int i = 0; i < elencoRisorse.size(); i++)
+		          {
+			         Risorsa r = elencoRisorse.get(i);
+			         ris.append("\t\t" + (i+1) + ")"+ r.toString());
+		          }
+		       }
+		   }
+		   else
+		   {
+			   ris.append(String.format(DESCRIZIONE_CATEGORIA_COMPOSTA, nomeCategoria));
+			   
+			   if(elencoSottoCategorie.size() == 0)
+				    ris.append(ELENCO_SOTTOCATEGORIE_VUOTO);
+			   else
+			   {
+			      for(int i = 0; i < elencoSottoCategorie.size(); i++)
+		          {
+			         SottoCategoria s = elencoSottoCategorie.get(i);
+			         ris.append("\t"+ (i+1) + ")"+ s.toString());
+		          }
+		        }
+		    }
+		   
+		   return ris.toString();
+	   }
+	   */
+	   
+}
---
+++ ProcessHandler.java
@@ -0,0 +1,164 @@
+package interazione;
+
+import java.io.Serializable;
+
+import java.util.ArrayList;
+
+import dominio.Categoria;
+import dominio.Risorsa;
+
+import logica.Archivio;
+import logica.ArchivioPrestiti;
+
+import logica.Utente;
+import utility.Costanti;
+import utility.InputDati;
+
+public abstract class ProcessHandler implements Serializable
+{  
+	//private static final long serialVersionUID = 1L;
+	
+	protected Archivio arc;
+    protected ArchivioPrestiti ap;
+    
+    /*
+    public ProcessHandler(Archivio arc, ArchivioPrestiti ap)
+    {  
+    	this.arc = arc;
+    	this.ap = ap;
+    }
+
+    public abstract Utente accesso();
+  
+    public ArrayList <Risorsa> ricercaRisorsa(Utente ut)
+    {
+       	Categoria c = null;
+       	
+   	 	System.out.printf(Costanti.CONTENUTO_ARC, arc.stampaElencoCategorie());
+   	 	
+   	    int num1 = InputDati.leggiIntero(Costanti.INS_NUMERO_CAT_RICERCA, Costanti.NUM_MINIMO, (arc.getElencoCategorie()).size());
+   	    c = (arc.getElencoCategorie()).get(num1-Costanti.NUM_MINIMO);
+   	 		
+   	 	if(c.getNome().equalsIgnoreCase("Libri"))
+				return ricercaRisorsaLibri(ut, c);
+   	 	else if(c.getNome().equalsIgnoreCase("Film"))
+				return ricercaRisorsaFilm(ut, c);
+   	 	else
+   	 		return null;
+    }
+    */
+    
+    public ArrayList <Risorsa> ricercaRisorsaLibri(Utente ut, Categoria c)
+    {
+   	    int numScelta = InputDati.leggiIntero(Costanti.AVVIO_RICERCA_LIBRI, Costanti.NUM_MINIMO, Costanti.NUM_MASSIMO_RICERCA);
+   	    Object o = null;
+   	    String s = "";
+   	
+   	    switch(numScelta)
+   	    {
+	    	   case 1: o = InputDati.leggiStringa(Costanti.INS_PAROLA_TITOLO_RISORSA);
+	    			   s = Categoria.RIC_PER_TITOLO;
+	    			   break;
+	    		
+	    	   case 2: o = InputDati.leggiStringa(Costanti.INS_COGNOME_AUTORE_LIBRO); 
+	    		       s = Categoria.RIC_PER_AUTORE_I;
+	    		       break;
+	       
+	    	   case 3: o = InputDati.leggiStringa(Costanti.INS_GENERE_RISORSA);
+	    		       s = Categoria.RIC_PER_GENERE;
+	    		       break;
+	    	   
+	    	   case 4: o = InputDati.leggiIntero(Costanti.INS_ANNOPUB_RISORSA);
+	    	   		   s = Categoria.RIC_PER_ANNOPUB;
+	    	   		   break;
+	    		
+	    	   case 5: o = InputDati.leggiStringa(Costanti.INS_CASAED_LIBRO);
+	    	   		   s = Categoria.RIC_PER_CASAED;
+	    	   		   break;
+   	    }  
+	    
+   	    return ut.ricercaRisorsa(c, o, s);
+    }
+   
+    public ArrayList <Risorsa> ricercaRisorsaFilm(Utente ut, Categoria c)
+    {
+   	    int numScelta = InputDati.leggiIntero(Costanti.AVVIO_RICERCA_FILM, Costanti.NUM_MINIMO, Costanti.NUM_MASSIMO_RICERCA);
+   	    Object o = null;
+   	    String s = "";
+   	
+   	    switch(numScelta)
+   	    {
+   	     	case 1: o = InputDati.leggiStringa(Costanti.INS_PAROLA_TITOLO_RISORSA);
+   	    			s = Categoria.RIC_PER_TITOLO;
+	    			    break;
+	    		
+   	      	case 2: o = InputDati.leggiStringa(Costanti.INS_COGNOME_REGISTA_FILM); 
+	    		        s = Categoria.RIC_PER_REGISTA;
+	    		        break;
+	       
+   	        case 3: o = InputDati.leggiStringa(Costanti.INS_COGNOME_ATTORE_FILM);
+	    		        s = Categoria.RIC_PER_ATTORE_I;
+	                    break;
+	    	   
+   	      	case 4: o = InputDati.leggiIntero(Costanti.INS_ANNOPUB_RISORSA);
+	                    s = Categoria.RIC_PER_ANNOPUB;
+	                    break;
+	    		
+   	       	case 5: o = InputDati.leggiStringa(Costanti.INS_GENERE_RISORSA);
+   	    		    s = Categoria.RIC_PER_GENERE;
+   	    			break;
+   	    }  
+	    
+   	    return ut.ricercaRisorsa(c, o, s);
+    }
+    
+   /*
+   public String ricercaRisorsaFormatoStringa(ArrayList <Risorsa> elencoRisorse)
+   {
+  	    StringBuffer ris = new StringBuffer();
+  	    ris.append(Costanti.INTESTAZIONE_RICERCA_RISORSE);
+  	    
+  	    if(elencoRisorse.size() == Costanti.VUOTO)
+  	    	    ris.append(Costanti.RICERCA_VUOTA);
+  	    
+		for(int i = 0; i < elencoRisorse.size(); i++)
+		{
+			Risorsa r = elencoRisorse.get(i);
+			ris.append("\t\t" + (i+1) + ")" + r.toString());
+		}
+		
+		return ris.toString();
+   }
+   
+   public String valutazioneDisponibilita(Utente ut)
+   {
+     	ArrayList <Risorsa> risorseTrovate = ricercaRisorsa(ut);
+	    String s = "";
+     	System.out.println(ricercaRisorsaFormatoStringa(risorseTrovate));
+   	    
+     	if(risorseTrovate.size() != Costanti.VUOTO)
+     	{
+     	   int num = InputDati.leggiIntero(Costanti.RICHIESTA_DIGITAZIONE_VALUTAZIONE, Costanti.NUM_MINIMO, risorseTrovate.size());
+     	   Risorsa r = risorseTrovate.get(num-Costanti.NUM_MINIMO);
+      	
+  	       if(ut.valutazioneDisponibilita(ap, r))
+     	        s += Costanti.RISORSA_DISPONIBILE;
+     	   else
+   		    s += Costanti.RISORSA_NON_DISPONIBILE;
+     	}
+     	else 
+     	      s += Costanti.NO_VALUTAZIONE;
+     	
+     	return s;
+   }
+   
+   public boolean richiestaLogout()
+   {
+      	if(InputDati.leggiUpperChar(Costanti.RICHIESTA_LOGOUT, "SN") == 'S')
+    		     return true;
+      	else
+    		     return false;
+   }
+   */
+    
+}
