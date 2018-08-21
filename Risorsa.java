b9d38371a6b548d136abb6bebb2715cd8b24d324
rk59W18a <42214911+rk59W18a@users.noreply.github.com>
2018-08-21 16:06:22 +0200

    Add files via upload

---
+++ Risorsa.java
@@ -0,0 +1,53 @@
+package dominio.parte2.punto3.prima;
+
+import java.io.Serializable;
+
+public abstract class Risorsa implements Serializable
+{
+	//private static final long serialVersionUID = 1L;
+	
+	protected String titolo;
+	
+	//private int numLicenze;
+	//private String genere;
+	//private int annoPub;
+	//private String lingua;
+    
+    /*
+    public Risorsa(String t, int lic, String g, int ap, String l)
+    {
+    	 this.titolo = t;
+    	 this.numLicenze = lic;
+    	 this.genere = g;
+    	 this.annoPub = ap;
+    	 this.lingua = l;
+    }
+    
+    public String getTitolo()
+    {
+    	 return titolo;
+    }
+    
+    public int getNumLicenze() 
+    {
+    	 return numLicenze;
+    }
+    
+    public String getGenere()
+    {
+    	 return genere;
+    }
+    
+    public int getAnnoPub()
+    {
+    	 return annoPub;
+    }
+    
+    public String getLingua()
+    {
+    	 return lingua;
+    }
+    
+    public abstract String toString();
+    */
+}
